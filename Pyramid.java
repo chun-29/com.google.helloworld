7c410ed06cf3af6aa6fed18b6b1bb9f5733f8d91
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-18 19:23:00 +0800

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -6,18 +6,21 @@
 import java.util.Scanner;
 public class Pyramid {
     public static void main(String[] args) {
-	    int m;
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter m: ");
-		int n=scan.nextInt();
-		for(int i=1;i<=n;i++){
-		    for(int k=1;k<=n-i;k++){
-			    System.out.printf(" ");
-			}
-			for(int j=1;j<=(2*i-1);j++){
-			    System.out.printf("*");
-			}
-			System.out.println();
-		}
-	}   
+        //the number of layers of the pyramid
+        int m;
+        Scanner scan = new Scanner(System.in);
+	System.out.println("Enter m: ");
+	int n=scan.nextInt();
+	for(int i=1;i<=n;i++){
+            //控制空格的个数
+	    for(int k=1;k<=n-i;k++){
+	        System.out.printf(" ");
+	    }
+	    //控制*的个数
+	    for(int j=1;j<=(2*i-1);j++){
+	        System.out.printf("*");
+	    }
+	    System.out.println();
+	}
+    }   
 }
