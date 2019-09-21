cd98ac216ea03ed5cfcb7e311ff7010f4b8563ff
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 17:07:01 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@
 * Date:        2019-9-9
 */
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 	//Output Hello World!
 	System.out.println("Hello World!");
     }
