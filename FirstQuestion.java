5ade241db4c2687fe8adb1c7194e62d5c3c3920d
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 17:28:38 +0800

    Update FirstQuestion.java

--- FirstQuestion.java
+++ FirstQuestion.java
@@ -1,5 +1,10 @@
-public class FirstQuestion{
-    public static void main(String[] args){
-	    System.out.println("34+30="+34+30);
-	}
+/**
+* Description: Output "34 + 30 =" + 34 + 30
+* Author:      chun
+* Date:        2019-9-15
+*/
+public class FirstQuestion {
+    public static void main(String[] args) {
+	System.out.println("34 + 30 =" + 34 + 30);
+    }
 }
