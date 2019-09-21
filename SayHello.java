e3409b49aa78610f636dc72400e44018a87013e4
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 16:27:47 +0800

    Update SayHello.java

--- SayHello.java
+++ SayHello.java
@@ -1,16 +1,25 @@
+/**
+* Description: Say Hello and Calculate the sum of 1 to 10 
+* Author:      chun
+* Date:        2019-9-11
+*/
 public class SayHello{
     public static void main(String[] args){
-	    System.out.println("Below is the function of sayhello");
-		SayHello();
+	System.out.println("Below is the function of sayhello");
+	SayHello();
+    }
+    public static void SayHello(){
+	System.out.println("say hello!" + Sum());
+    }
+    public static int Sum(){
+	int i;
+	//给sum赋初值为零
+	int sum = 0;
+	//利用for循环语句，在i<=100的条件下，i递增
+        for(i = 1;i <= 100;i++){
+	    //1-100累加
+	    sum = sum+i;
 	}
-	public static void SayHello(){
-	    System.out.println("say hello!"+Sum());
-	}
-	public static int Sum(){
-	    int i,sum=0;
-		for(i=1;i<=100;i++){
-		    sum=sum+i;
-		}
-		return sum;
-	}
+	return sum;
+    }
 }
