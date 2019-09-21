7ac7f90e489aaf9144772f8683c2805a9a7a4d16
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 16:54:35 +0800

    Update SayHello.java

--- SayHello.java
+++ SayHello.java
@@ -16,9 +16,9 @@ public class SayHello{
 	//给sum赋初值为零
 	int sum = 0;
 	//利用for循环语句，在i<=100的条件下，i递增
-        for(i = 1;i <= 100;i++){
+        for(i = 1;i < = 100;i++){
 	    //1-100累加
-	    sum = sum+i;
+	    sum = sum + i;
 	}
 	return sum;
     }
