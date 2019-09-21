8daed5bd9150bfa733e5319363a69778add6d8f0
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 17:13:20 +0800

    Update SayHi.java

--- SayHi.java
+++ SayHi.java
@@ -3,18 +3,20 @@
 * Author:      chun
 * Date:        2019-9-11
 */
-public class SayHi{
-    public static void main(String[] args){
+public class SayHi {
+    public static void main(String[] args) {
 	SayHi sayHi=new SayHi();
 	sayHi.sayHi(sayHi.sum());
     }
-    public void sayHi(int sum){
-	//
+    public void sayHi(int sum) {
 	System.out.println("Say Hi!" + sum());
     }
-    public int sum(){
+    public int sum() {
+	//给sum赋初值为零
 	int sum = 0;
-        for(int i = 0;i < = 100;i++){
+	//在i<=100的前提条件下，i递增
+        for(int i = 0;i < = 100;i++) {
+	    //将1-100的累加和赋值给sum
 	    sum + = i;
 	}
 	return sum;
