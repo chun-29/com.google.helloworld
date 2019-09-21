a9cdd87066b8a6d8b4de2214dd4677d9e30984bc
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 16:52:51 +0800

    Update SayHi.java

--- SayHi.java
+++ SayHi.java
@@ -1,16 +1,22 @@
+/**
+* Description: Say Hi and Calculate the sum of 1 to 10
+* Author:      chun
+* Date:        2019-9-11
+*/
 public class SayHi{
     public static void main(String[] args){
-	    SayHi sayHi=new SayHi();
-		sayHi.sayHi(sayHi.sum());
+	SayHi sayHi=new SayHi();
+	sayHi.sayHi(sayHi.sum());
+    }
+    public void sayHi(int sum){
+	//
+	System.out.println("Say Hi!" + sum());
+    }
+    public int sum(){
+	int sum = 0;
+        for(int i = 0;i < = 100;i++){
+	    sum + = i;
 	}
-	public void sayHi(int sum){
-	    System.out.println("Say Hi!"+sum());
-	}
-	public int sum(){
-	    int sum=0;
-		for(int i=0;i<=100;i++){
-		    sum+=i;
-		}
-		return sum;
-	}
+	return sum;
+    }
 }
