3c2ebcc49522b9abc283cb0a131ff22a55961ad5
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 18:21:25 +0800

    Update ThirdQuestion.java

--- ThirdQuestion.java
+++ ThirdQuestion.java
@@ -1,26 +1,37 @@
-public class ThirdQuestion{
-    public static void main(String[] args){
-	    int res,n1=12,n2=24,n3=16,n4=3;
-		double d,v1=10.0,v2=3.1416;
-		res=n1/n4;
-	    System.out.println("res=n1/n4="+res);
-		d=n1/n4;
-		System.out.println("d=n1/n4="+d);
-		res=n3/n4;
-		System.out.println("res=n3/n4="+res);
-		d=n3/n4;
-		System.out.println("d=n3/n4="+d);
-		d=v1/n4;
-		System.out.println("d=v1/n4="+d);
-		d=v1/v2;
-		System.out.println("d=v1/v2="+d);
-		res=n1/n2;
-		System.out.println("res=n1/n2="+res);
-		d=(double)n1/n2;
-		System.out.println("d=(double)n1/n2="+d);
-		d=n1/(double)n2;
-		System.out.println("d=n1/(double)n2="+d);
-		d=(double)(n1/n2);
-		System.out.println("d=(double)(n1/n2)="+d);
-	}
+/**
+* Description: Calculate and output the values of the ten formulas give
+* Author:      chun
+* Date:        2019-9-15
+*/
+public class ThirdQuestion {
+    public static void main(String[] args) {
+	int res;
+	int n1 = 12;
+	int n2 = 24;
+	int n3 = 16;
+	int n4 = 3;
+	double d;
+	double v1 = 10.0;
+	double v2 = 3.1416;
+	res = n1 / n4;
+	System.out.println("res = n1 / n4 =" + res);
+	d = n1 / n4;
+	System.out.println("d = n1 / n4 =" + d);
+	res = n3 / n4;
+	System.out.println("res = n3 / n4 =" + res);
+	d = n3 / n4;
+	System.out.println("d = n3 / n4 =" + d);
+	d = v1 / n4;
+	System.out.println("d = v1 / n4 =" + d);
+	d = v1 / v2;
+	System.out.println("d = v1 / v2 = " + d);
+	res = n1 / n2;
+	System.out.println("res = n1 / n2 =" + res);
+	d = (double)n1 / n2;
+	System.out.println("d = (double)n1 / n2 =" + d);
+	d = n1 / (double)n2;
+	System.out.println("d = n1 / (double)n2 =" + d);
+	d = (double)(n1 / n2);
+	System.out.println("d = (double)(n1 / n2) =" + d);
+    }
 }
