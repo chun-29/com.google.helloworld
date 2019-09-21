9e8f5a317952382f79ddec9f4f4e2aceb916629f
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-21 17:09:16 +0800

    Update MulTable.java

--- MulTable.java
+++ MulTable.java
@@ -4,11 +4,11 @@
 * Date:        2019-9-16
 */
 public class MulTable {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //控制九九乘法表只有九行    
-	for(int i = 1;i < = 9;i++){
+	for(int i = 1;i < = 9;i++) {
 	    //控制第i行有i条乘法算式
-            for(int j = 1;j < = i;j++){
+            for(int j = 1;j < = i;j++) {
 	        System.out.printf(i + "*" + j + "=" + j * i + " ");
 	    }
 	    System.out.println(" ");
