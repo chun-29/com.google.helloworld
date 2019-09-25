ea784400389347cf80acdce6dee3241e80bf8f1a
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-25 09:36:01 +0800

    Update Age.java

--- Age.java
+++ Age.java
@@ -5,18 +5,21 @@
 */
 import java.util.Scanner;
 public class Age {
-	public static void main(String[] args) {
-		System.out.println("Enter age: ");
-	    Scanner scan = new Scanner(System.in);
-	    int age = scan.nextInt();
-		age = age / 18;
-	    switch(age) {
-			case 0:
-		        System.out.println("young sister");
-			    break;
-		    default:
-		        System.out.println("old sister");
-			    break;
-	    }
-	}	
+    public static void main(String[] args) {
+	System.out.println("Enter age: ");
+	Scanner scan = new Scanner(System.in);
+	int age = scan.nextInt();
+	// 以18岁为分界，做判断
+	age = age / 18;
+	switch(age) {
+            // 小于18岁的称呼小姐姐
+	    case 0:
+		System.out.println("young sister");
+	        break;
+	    // 大于等于18岁的称呼大姐姐
+	    default:
+		System.out.println("old sister");
+	        break;
+	}
+    }	
 }
