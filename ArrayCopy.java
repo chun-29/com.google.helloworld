faf404f3b150d79d1b240efaeca99744db85ce74
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-29 23:55:51 +0800

    Update ArrayCopy.java

--- ArrayCopy.java
+++ ArrayCopy.java
@@ -5,11 +5,11 @@
 */
 public class ArrayCopy {
     public static void main(String[] args) {
-	    int array1[] = {3,4,5,7,8,10,11,15,13,11};
-		int array2[] = {0,0,0,0,0,0,0,0,0,0};
-		System.arraycopy(array1,0,array2,1,6);
-		System.out.println("array2: ");
-		for(int i = 0;i < array2.length;i++)
-        	System.out.print(array2[i] + " ");
-	}
+	int array1[] = {3,4,5,7,8,10,11,15,13,11};
+	int array2[] = {0,0,0,0,0,0,0,0,0,0};
+	System.arraycopy(array1,0,array2,1,6);
+	System.out.println("array2: ");
+	for(int i = 0;i < array2.length;i++)
+            System.out.print(array2[i] + " ");
+    }
 }
