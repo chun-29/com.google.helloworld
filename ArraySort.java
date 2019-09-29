be8dcdf55424cdcdb6915dd5138200eb907cb246
chun-29 <54794957+chun-29@users.noreply.github.com>
2019-09-29 23:54:50 +0800

    Update ArraySort.java

--- ArraySort.java
+++ ArraySort.java
@@ -6,9 +6,9 @@
 import java.util.Arrays;
 public class ArraySort {
     public static void main(String args[]) {
-		int array[] = {3,2,6,4,5,9,8,7};
-		Arrays.sort(array);
-		for(int i = 0;i < array.length;i++)
-		    System.out.println(array[i] + " ");
-	}
+	int array[] = {3,2,6,4,5,9,8,7};
+	Arrays.sort(array);
+	for(int i = 0;i < array.length;i++)
+	    System.out.println(array[i] + " ");
+    }
 }
