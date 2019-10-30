b078d3fc85b5e6fba9e7348234fa88786d3a1c65
chun-29 <1285166304@qq.com>
2019-10-30 21:25:02 +0800

    created a new user class

---
+++ DefaultValue.java
@@ -0,0 +1,18 @@
+/**
+ * @program: usermanagerment
+ * @Description: Custom user name and password
+ * @author: chun
+ * @date: 2019-10-30 
+ */
+package com.google.user;
+
+/**
+ * @author Administrator
+ * @date   2019-10-30
+ */
+public class DefaultValue {
+	
+	//final是不可修改的意思，自定义用户名和用户密码
+	public final static String USERNAME = "jame";
+	public final static String USERPASSWORD = "123456";
+}
